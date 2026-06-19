af65876e547e268bd5b28dd46f1d68cdab25ca24
johnbutlergames <johnbutlergames777@gmail.com>
2026-06-18 18:53:35 -0700

    initial commit

---
+++ canvas.js
@@ -0,0 +1,9 @@
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+
+function resize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+resize();
+window.addEventListener("resize", resize);
